c76fbd0b0ea16b27d25afb4262f614c25640f0dc
aruballo <x4tr56x@gmail.com>
2016-07-12 23:57:31 -0700

    Add life logic and Game Over screen; move app.js code into engine.js

--- js/engine.js
+++ js/engine.js
@@ -23,13 +23,37 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime;
+        lastTime,
+        gameEnded = false,
+        grunt1,
+        grunt2,
+        grunt3,
+        allEnemies,
+        player;
+
+
     var playerImages = [
         'images/char-boy.png',
         'images/char-cat-girl.png',
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
         'images/char-princess-girl.png'
+    ];
+
+    var allImages = [
+        'images/stone-block.png',
+        'images/water-block.png',
+        'images/grass-block.png',
+        'images/enemy-bug.png',
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Title.png',
+        'images/Heart.png',
+        'images/Heart2.png',
+        'images/Heart3.png',
     ];
 
     canvas.width = 505;
@@ -64,10 +88,25 @@ var Engine = (function(global) {
         /* Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          */
-        win.requestAnimationFrame(main);
+        if(!gameEnded){
+            win.requestAnimationFrame(main);
+        }
+
     }
 
+    // Bring up the main menu with charater choices
     function initMainMenu() {
+
+        grunt1 = new Enemy(1,43,70);
+        grunt2 = new Enemy(1,126,120);
+        grunt3 = new Enemy(1,209,180);
+        allEnemies = [];
+
+        allEnemies[0] = grunt1;
+        allEnemies[1] = grunt2;
+        allEnemies[2] = grunt3;
+
+        player = new Player(205,375,1);
 
         renderBackground();
         ctx.drawImage(Resources.get('images/Title.png'), 0, 80, 500, 420);
@@ -109,6 +148,8 @@ var Engine = (function(global) {
 
         if(x > 0 && y > 460){
             initGame();
+            player.updateScore(0);
+            player.updateLife(3);
         }
 
     }
@@ -123,9 +164,34 @@ var Engine = (function(global) {
         this.backgroundAudio.volume = .05;
         this.backgroundAudio.load();
         this.backgroundAudio.play();
+        // This listens for key presses and sends the keys to your
+        // Player.handleInput() method. You don't need to modify this.
+        document.addEventListener('keyup', keyUpHandler, false);
+        canvas.removeEventListener('click', characterClick, false);
         reset();
         lastTime = Date.now();
         main();
+    }
+
+    function gameOver() {
+        gameEnded = true;
+        renderBackground();
+        this.backgroundAudio.pause();
+        this.backgroundAudio.currentTime = 0;
+        ctx.fillStyle = "red";
+        ctx.font = "bold 64px Serif";
+        ctx.fillText("GAME OVER!", 40,430);
+        ctx.font = "bold 32px Serif";
+        ctx.fillText("Press Any Key", 10, 500);
+        ctx.fillText("To Continue", 315, 500);
+        doc.addEventListener('keydown', function(e){
+            if(gameEnded == true){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                gameEnded = false;
+                document.removeEventListener("keyup", keyUpHandler, false)
+                initMainMenu();
+            }
+        }, false);
     }
 
     /* This function is called by main (our game loop) and itself calls all
@@ -204,6 +270,11 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        if(player.life <= 0){
+            gameOver();
+        }
+
+
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
@@ -219,22 +290,238 @@ var Engine = (function(global) {
         // noop
     }
 
+    function keyUpHandler(e) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
+        };
+
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
+
+    // Enemies our player must avoid
+    function Enemy(x, y, speed) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
+
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    };
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    Enemy.prototype.update = function(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+
+        // Calculate initial movement
+        var delta = this.x + this.speed * dt;
+
+        // Boundaries for player hit detection
+        var lowX = player.x - 35;
+        var highX = player.x + 35;
+        var lowY = player.y - 15;
+        var highY = player.y + 15;
+
+        // Player hit trigger
+        if(lowX <= delta && highX >= delta && lowY <= this.y && highY >= this.y){
+            player.damaged();
+        }
+
+        // If this enemy goes offscreen, reset his position
+        // and give him a new speed. Otherwise continue forward
+        if(delta > 540){
+
+            var speedMultiplier = Math.random() * (3 - 1) + 1;
+            var incOrDec = Math.floor(Math.random() * (4 - 1) + 1);
+            var newSpeed = 0;
+
+            if(incOrDec >= 2){
+                newSpeed = this.speed * speedMultiplier;
+            }
+            else{
+                newSpeed = this.speed / speedMultiplier;
+            }
+
+            if(newSpeed > 250){
+                newSpeed = 250;
+            }
+            else if(newSpeed < 100){
+                newSpeed = 100;
+            }
+
+            this.speed = newSpeed;
+            this.x = -90;
+        }
+        else{
+            this.x = delta;
+        }
+
+
+    };
+
+    // Draw the enemy on the screen, required method for game
+    Enemy.prototype.render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+
+    // Now write your own player class
+    // This class requires an update(), render() and
+    // a handleInput() method.
+    function Player(x, y, speed){
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-horn-girl.png';
+        this.speed = speed;
+        this.score = 0;
+        this.crossed = new SoundPool(10);
+        this.crossed.init("crossed");
+        this.life = 3;
+        this.hit = new SoundPool(20);
+        this.hit.init("hit");
+    }
+
+    Player.prototype.render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+
+    // Process player input
+    Player.prototype.handleInput = function(key){
+
+        // Variables later used to determine if
+        // position to move to is valid
+        var originalX = this.x;
+        var originalY = this.y;
+        var newX = 0;
+        var newY = 0;
+
+       // 101 and 83 were determined to be the
+       // row and column sizes
+       if (key == 'left'){
+            newX = this.x - 101;
+            newY = originalY;
+        }
+        else if (key == "right"){
+            newX = this.x + 101;
+            newY = originalY;
+
+        }
+        else if (key == "up"){
+            newY = this.y - 83;
+            newX = originalX;
+        }
+        else if (key == "down"){
+            newY = this.y + 83;
+            newX = originalX;
+        }
+        // All other inputs should be ignored
+        else {
+            return;
+        }
+
+        // If the next x movement is outside
+        // the boundaries, do nothing
+        if (newX < 0 || newX > 498){
+            this.x = originalX;
+        }
+        else{
+            this.x = newX;
+        }
+
+        // Check if the next move collides
+        // with an enemy
+        for(var i = 0; i < allEnemies.length; i++){
+            var lowX = newX - 15;
+            var highX = newX + 15;
+
+            var lowY = newY - 15;
+            var highY = newY + 15;
+            if((lowX <= allEnemies[i].x  && highX >= allEnemies[i].x)
+                && (lowY <= allEnemies[i].y && highY >= allEnemies[i].y)){
+                this.damaged();
+                return;
+            }
+        }
+
+        // If the player crossed the blue water,
+        // reset and increase the score
+        if (newY < 0){
+            this.scored();
+        }
+        else if (newY > 405){
+            this.y = originalY;
+        }
+        else{
+            this.y = newY;
+        }
+
+    }
+
+    // A white rectangle is drawn prior to updating the score
+    // in order to prevent text overlap.
+    Player.prototype.updateScore = function(score){
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0,200,200);
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "blue";
+        ctx.fillText("Score: " + score,10,50);
+    }
+
+    // Increase score
+    Player.prototype.scored = function(){
+        this.crossed.get();
+        this.score = this.score + 10;
+        this.updateScore(this.score);
+        this.reset();
+    }
+
+    // Decrease score
+    Player.prototype.damaged = function(){
+        this.hit.get();
+        this.life--;
+        this.score = this.score - 10;
+        this.updateScore(this.score);
+        this.updateLife(this.life);
+        this.reset();
+    }
+
+    Player.prototype.updateLife = function(life){
+        var heartsImage = "";
+
+        if(life == 0){
+
+        }
+
+        ctx.fillStyle = "white";
+        ctx.fillRect(400, 0,300,300);
+        ctx.drawImage(Resources.get(allImages[life + 9]), 400, 0);
+    }
+
+    // Not sure what the purpose of this is.
+    Player.prototype.update = function(){
+    }
+
+    // Set player back to original starting position
+    Player.prototype.reset = function(){
+        this.x = 205;
+        this.y = 375;
+        this.speed = 1;
+    }
+
+
+
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
      * all of these images are properly loaded our game will start.
      */
-    Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/char-cat-girl.png',
-        'images/char-horn-girl.png',
-        'images/char-pink-girl.png',
-        'images/char-princess-girl.png',
-        'images/Title.png'
-    ]);
+    Resources.load(allImages);
     Resources.onReady(initMainMenu);
 
     /* Assign the canvas' context object to the global variable (the window
